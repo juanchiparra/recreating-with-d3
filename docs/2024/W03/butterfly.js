5f44f92e44dd780e6dd855d4ae13515c17297898
Juanchi Parra <juanchiparra@gmail.com>
2024-10-10 00:06:21 -0400

    Create butterfly.js

---
+++ docs/2024/W03/butterfly.js
@@ -0,0 +1,15 @@
+export const data = [
+	{ name: "Freedom", dnc: 226, rnc: 67 },
+	{ name: "Woman, women", dnc: 205, rnc: 96 },
+	{ name: "Future", dnc: 199, rnc: 70 },
+	{ name: "Democracy", dnc: 106, rnc: 15 },
+	{ name: "God", dnc: 101, rnc: 299 },
+	{ name: "Mother", dnc: 96, rnc: 30 },
+	{ name: "Joy", dnc: 70, rnc: 6 },
+	{ name: "Groceries", dnc: 10, rnc: 50 },
+	{ name: "Weird", dnc: 8, rnc: 6 },
+	{ name: "Inflation", dnc: 10, rnc: 65 },
+	{ name: "Liberal", dnc: 4, rnc: 16 },
+	{ name: "Assassin(ation)", dnc: 3, rnc: 15 },
+	{ name: "Woke", dnc: 3, rnc: 11 }
+];
