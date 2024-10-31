21f6fdb416cff49468db4978f4b57c42de10cf55
Juanchi Parra <juanchiparra@gmail.com>
2024-10-30 20:50:10 -0400

    Create treemap.js

---
+++ docs/2024/W06/treemap.js
@@ -0,0 +1,366 @@
+export const data = {
+    name: "COUNTRIES",
+    children: [
+        {
+            "initials": "UKR",
+            "name": "UKRAINE",
+            "value": 2247,
+            "percent": "45.33%",
+            "individuals": 1774,
+            "entities": 473,
+            "sanctions": 2247,
+            "travel_bans": 1768
+        },
+        {
+            "initials": "IRN",
+            "name": "IRAN",
+            "value": 491,
+            "percent": "9.9%",
+            "individuals": 295,
+            "entities": 196,
+            "sanctions": 491,
+            "travel_bans": 263
+        },
+        {
+            "initials": "SYR",
+            "name": "SYRIAN ARAB REPUBLIC",
+            "value": 407,
+            "percent": "8.2%",
+            "individuals": 320,
+            "entities": 87,
+            "sanctions": 407,
+            "travel_bans": 280
+        },
+        {
+            "initials": "TAQA",
+            "name": "AL-QAEDA",
+            "value": 323,
+            "percent": "6.5%",
+            "individuals": 244,
+            "entities": 79,
+            "sanctions": 323,
+            "travel_bans": 244
+        },
+        {
+            "initials": "BLR",
+            "name": "BELARUS",
+            "value": 298,
+            "percent": "6.0%",
+            "individuals": 261,
+            "entities": 37,
+            "sanctions": 298,
+            "travel_bans": 0
+        },
+        {
+            "initials": "PRK",
+            "name": "NORTH KOREA",
+            "value": 218,
+            "percent": "4.4%",
+            "individuals": 143,
+            "entities": 75,
+            "sanctions": 218,
+            "travel_bans": 142
+        },
+        {
+            "initials": "HR",
+            "name": "HUMAN RIGHTS",
+            "value": 150,
+            "percent": "3.0%",
+            "individuals": 117,
+            "entities": 33,
+            "sanctions": 150,
+            "travel_bans": 117
+        },
+        {
+            "initials": "AFG",
+            "name": "AFGHANISTAN",
+            "value": 140,
+            "percent": "2.8%",
+            "individuals": 135,
+            "entities": 5,
+            "sanctions": 140,
+            "travel_bans": 135
+        },
+        {
+            "initials": "MMR",
+            "name": "MYANMAR/BURMA",
+            "value": 124,
+            "percent": "2.6%",
+            "individuals": 103,
+            "entities": 21,
+            "sanctions": 124,
+            "travel_bans": 103
+        },
+        {
+            "initials": "IRQ",
+            "name": "IRAQ",
+            "value": 76,
+            "percent": "1.54%",
+            "individuals": 66,
+            "entities": 10,
+            "sanctions": 76,
+            "travel_bans": 0
+        },
+        {
+            "initials": "COD",
+            "name": "DEMOCRATIC REPUBLIC OF CONGO",
+            "value": 74,
+            "percent": "1.5%",
+            "individuals": 64,
+            "entities": 10,
+            "sanctions": 74,
+            "travel_bans": 64
+        },
+        {
+            "initials": "VEN",
+            "name": "VENEZUELA",
+            "value": 54,
+            "percent": "1.1%",
+            "individuals": 54,
+            "entities": 0,
+            "sanctions": 54,
+            "travel_bans": 54
+        },
+        {
+            "initials": "LBY",
+            "name": "LIBYA",
+            "value": 52,
+            "percent": "1.05%",
+            "individuals": 35,
+            "entities": 17,
+            "sanctions": 52,
+            "travel_bans": 35
+        },
+        {
+            "initials": "TERR",
+            "name": "TERRORIST GROUPS",
+            "value": 37,
+            "percent": "0.75%",
+            "individuals": 15,
+            "entities": 22,
+            "sanctions": 37,
+            "travel_bans": 0
+        },
+        {
+            "initials": "TUN",
+            "name": "TUNISIA",
+            "value": 30,
+            "percent": "0.61%",
+            "individuals": 30,
+            "entities": 0,
+            "sanctions": 30,
+            "travel_bans": 0
+        },
+        {
+            "initials": "CHEM",
+            "name": "CHEMICAL WEAPONS",
+            "value": 28,
+            "percent": "0.56%",
+            "individuals": 25,
+            "entities": 3,
+            "sanctions": 28,
+            "travel_bans": 25
+        },
+        {
+            "initials": "NIC",
+            "name": "NICARAGUA",
+            "value": 24,
+            "percent": "0.48%",
+            "individuals": 21,
+            "entities": 3,
+            "sanctions": 24,
+            "travel_bans": 21
+        },
+        {
+            "initials": "EUAQ",
+            "name": "EU AUTONOMOUS AL-QAEDA",
+            "value": 21,
+            "percent": "0.42%",
+            "individuals": 15,
+            "entities": 6,
+            "sanctions": 21,
+            "travel_bans": 15
+        },
+        {
+            "initials": "RUS",
+            "name": "RUS",
+            "value": 20,
+            "percent": "0.40%",
+            "individuals": 19,
+            "entities": 1,
+            "sanctions": 20,
+            "travel_bans": 19
+        },
+        {
+            "initials": "SOM",
+            "name": "SOMALIA",
+            "value": 19,
+            "percent": "0.38%",
+            "individuals": 18,
+            "entities": 1,
+            "sanctions": 19,
+            "travel_bans": 18
+        },
+        {
+            "initials": "MDA",
+            "name": "MOLDOVA",
+            "value": 18,
+            "percent": "0.36%",
+            "individuals": 16,
+            "entities": 2,
+            "sanctions": 18,
+            "travel_bans": 11
+        },
+        {
+            "initials": "CYB",
+            "name": "CYBER-ATTACKS",
+            "value": 18,
+            "percent": "0.36%",
+            "individuals": 14,
+            "entities": 4,
+            "sanctions": 18,
+            "travel_bans": 14
+        },
+        {
+            "initials": "HAM",
+            "name": "HAMAS AND PIJ",
+            "value": 15,
+            "percent": "0.30%",
+            "individuals": 12,
+            "entities": 3,
+            "sanctions": 15,
+            "travel_bans": 12
+        },
+        {
+            "initials": "CAF",
+            "name": "CENTRAL AFRICAN REPUBLIC",
+            "value": 14,
+            "percent": "0.28%",
+            "individuals": 13,
+            "entities": 1,
+            "sanctions": 14,
+            "travel_bans": 13
+        },
+        {
+            "initials": "SDNZ",
+            "name": "SUDAN EU AUTONOMOUS",
+            "value": 12,
+            "percent": "0.24%",
+            "individuals": 6,
+            "entities": 6,
+            "sanctions": 12,
+            "travel_bans": 6
+        },
+        {
+            "initials": "SSD",
+            "name": "SOUTH SUDAN",
+            "value": 9,
+            "percent": "0.18%",
+            "individuals": 9,
+            "entities": 0,
+            "sanctions": 9,
+            "travel_bans": 9
+        },
+        {
+            "initials": "GNB",
+            "name": "GUINEA-BISSAU",
+            "value": 6,
+            "percent": "0.12%",
+            "individuals": 6,
+            "entities": 0,
+            "sanctions": 6,
+            "travel_bans": 6
+        },
+        {
+            "initials": "GTM",
+            "name": "GUATEMALA",
+            "value": 5,
+            "percent": "0.10%",
+            "individuals": 5,
+            "entities": 0,
+            "sanctions": 5,
+            "travel_bans": 5
+        },
+        {
+            "initials": "MLI",
+            "name": "MALI",
+            "value": 5,
+            "percent": "0.10%",
+            "individuals": 5,
+            "entities": 0,
+            "sanctions": 5,
+            "travel_bans": 5
+        },
+        {
+            "initials": "YEM",
+            "name": "YEMEN",
+            "value": 5,
+            "percent": "0.10%",
+            "individuals": 4,
+            "entities": 1,
+            "sanctions": 5,
+            "travel_bans": 4
+        },
+        {
+            "initials": "GIN",
+            "name": "GUINEA",
+            "value": 5,
+            "percent": "0.10%",
+            "individuals": 5,
+            "entities": 0,
+            "sanctions": 5,
+            "travel_bans": 5
+        },
+        {
+            "initials": "SDN",
+            "name": "SUDAN",
+            "value": 3,
+            "percent": "0.06%",
+            "individuals": 3,
+            "entities": 0,
+            "sanctions": 3,
+            "travel_bans": 3
+        },
+        {
+            "initials": "TUR",
+            "name": "TURKEY",
+            "value": 2,
+            "percent": "0.04%",
+            "individuals": 2,
+            "entities": 0,
+            "sanctions": 2,
+            "travel_bans": 2
+        },
+        {
+            "initials": "HTI",
+            "name": "HAÏTI",
+            "value": 1,
+            "percent": "0.02%",
+            "individuals": 1,
+            "entities": 0,
+            "sanctions": 1,
+            "travel_bans": 1
+        },
+        {
+            "initials": "ZWE",
+            "name": "ZIMBABWE",
+            "value": 1,
+            "percent": "0.02%",
+            "individuals": 0,
+            "entities": 1,
+            "sanctions": 1,
+            "travel_bans": 0
+        },
+        {
+            "initials": "BDI",
+            "name": "BURUNDI",
+            "value": 1,
+            "percent": "0.02%",
+            "individuals": 1,
+            "entities": 0,
+            "sanctions": 1,
+            "travel_bans": 1
+        }
+    ]
+};
+
