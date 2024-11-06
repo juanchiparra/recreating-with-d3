66a7cb6c0486cb4d1c0300d510fb4ad1e2a724e9
Juanchi Parra <juanchiparra@gmail.com>
2024-11-06 19:15:44 -0400

    Create bar-chart.js

---
+++ docs/2024/W07/bar-chart.js
@@ -0,0 +1,19 @@
+export const data = [
+	{ name: "Canarias", value: 3.1, value2: 8.6 },
+	{ name: "Andalucía", value: 5.2, value2: 8.3 },
+	{ name: "C. Valenciana", value: 2.8, value2: 7.3 },
+	{ name: "Extremadura", value: 4.6, value2: 6.9 },
+	{ name: "Murcia", value: 4.3, value2: 6.6 },
+	{ name: "C. La Mancha", value: 2.7, value2: 6.3 },
+	{ name: "C. León", value: 2.3, value2: 6.0 },
+	{ name: "Navarra", value: 2.3, value2: 5.7 },
+	{ name: "La Rioja", value: 2.8, value2: 5.4 },
+	{ name: "Asturias", value: 3.2, value2: 5.3 },
+	{ name: "Galicia", value: 2.3, value2: 5.2 },
+	{ name: "Cantabria", value: 2.2, value2: 4.6 },
+	{ name: "Catalunya", value: 1.7, value2: 4.3 },
+	{ name: "Baleares", value: 1.5, value2: 4.1 },
+	{ name: "Madrid", value: 1.6, value2: 3.5 },
+	{ name: "Aragón", value: 2.5, value2: 3.0 },
+	{ name: "Euskadi", value: 2.7, value2: 0 }
+]
