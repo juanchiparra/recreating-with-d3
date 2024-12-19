f33d2a63ae2b7ff01f18f26c8234005648b543ca
Juanchi Parra <juanchiparra@gmail.com>
2024-12-18 22:55:16 -0400

    Create stacked-bar.js

---
+++ docs/2024/W13/stacked-bar.js
@@ -0,0 +1,23 @@
+export const data1 = [
+    { category: '15-19', killed: 0.5, injured_low: 3.5, injured_high: 1 },
+    { category: '20-24', killed: 5, injured_low: 34, injured_high: 12 },
+    { category: '25-29', killed: 8, injured_low: 45, injured_high: 17 },
+    { category: '30-34', killed: 9, injured_low: 52, injured_high: 18 },
+    { category: '35-39', killed: 10, injured_low: 56, injured_high: 19 },
+    { category: '40-44', killed: 9, injured_low: 53, injured_high: 19 },
+    { category: '45-49', killed: 8, injured_low: 51, injured_high: 19 },
+    { category: '50-54', killed: 7, injured_low: 31, injured_high: 12 },
+    { category: '+55', killed: 3, injured_low: 14, injured_high: 5 }
+];
+
+export const data2 = [
+    { category: '15-19', killed: 0.05, injured_low: 0.32, injured_high: 0.075 },
+    { category: '20-24', killed: 0.5, injured_low: 3.4, injured_high: 1 },
+    { category: '25-29', killed: 0.6, injured_low: 3.6, injured_high: 1.2 },
+    { category: '30-34', killed: 0.4, injured_low: 3.2, injured_high: 0.9 },
+    { category: '35-39', killed: 0.4, injured_low: 3.2, injured_high: 0.9 },
+    { category: '40-44', killed: 0.4, injured_low: 3.4, injured_high: 1.1 },
+    { category: '45-49', killed: 0.5, injured_low: 3.6, injured_high: 1.1 },
+    { category: '50-54', killed: 0.3, injured_low: 2.5, injured_high: 0.5 },
+    { category: '55+', killed: 0.04, injured_low: 0.31, injured_high: 0.065 },
+];
