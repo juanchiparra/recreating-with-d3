f9d2a67450f8d81dbab14b6084d1efed7edff62c
Juanchi Parra <juanchiparra@gmail.com>
2025-02-12 18:38:27 -0400

    Create sankey.js

---
+++ docs/2025/W21/sankey.js
@@ -0,0 +1,12 @@
+export const data = [
+    { category1: "CABA", category2: "A. del Sur", value: 14182 },
+    { category1: "CABA", category2: "Europa", value: 100 },
+    { category1: "Buenos Aires", category2: "A. del Sur", value: 11640 },
+    { category1: "Buenos Aires", category2: "Europa", value: 3566 },
+    { category1: "Buenos Aires", category2: "A. del Norte", value: 3704 },
+    { category1: "Buenos Aires", category2: "A. Central y Caribe", value: 2000 },
+    { category1: "Resto", category2: "A. del Sur", value: 3770 },
+    { category1: "Resto", category2: "Europa", value: 304 },
+    { category1: "Resto", category2: "A. Central y Caribe", value: 1354 },
+    { category1: "Resto", category2: "África", value: 1 },
+];
