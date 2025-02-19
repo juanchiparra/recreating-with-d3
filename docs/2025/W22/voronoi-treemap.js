172cd1ba8ddd2e0d468f92634d0569044b9dff2a
Juanchi Parra <juanchiparra@gmail.com>
2025-02-19 11:29:00 -0400

    Create voronoi-treemap.js

---
+++ docs/2025/W22/voronoi-treemap.js
@@ -0,0 +1,82 @@
+export const data = {
+    name: "root",
+    children: [
+        {
+            name: "Yep ($1.6B)",
+            children: [
+                { name: "Sheriff Oversight", value: 1 },
+                { name: "Fire", value: 530 },
+                { name: "Police Accountability", value: 40 },
+                { name: "Juvenile Probation", value: 180 },
+                { name: "Adult Probation", value: 140 },
+                { name: "Emergency Management", value: 300 },
+                { name: "Police", value: 820 },
+            ]
+        },
+        {
+            name: "Segal ($2.6B)",
+            children: [
+                { name: "Airport", value: 2000 },
+                { name: "Library", value: 180 },
+                { name: "Economic & Workforce Dev.", value: 140 },
+                { name: "Building Inspection", value: 88 },
+                { name: "Planning", value: 55 },
+                { name: "War Memorial", value: 30 },
+                { name: "Arts", value: 29 },
+                { name: "Fine Arts Museums", value: 23 },
+                { name: "Rent Arbitration Board", value: 13 },
+                { name: "Asian Art Museum", value: 111 },
+                { name: "SubcategAcademy of Sciences", value: 7 },
+                { name: "Law Library", value: 1 },
+            ]
+        },
+        {
+            name: "John-Baptiste ($4.5B)",
+            children: [
+                { name: "Public Utilities", value: 2000 },
+                { name: "MTA", value: 1500 },
+                { name: "Public Works", value: 453 },
+                { name: "Recreation & Park", value: 254 },
+                { name: "Port", value: 156 },
+                { name: "Environment", value: 45 },
+            ]
+        },
+        {
+            name: "Modi ($6.6B)",
+            children: [
+                { name: "Public Health", value: 3200 },
+                { name: "Human Services", value: 1200 },
+                { name: "Homelessness", value: 846 },
+                { name: "Children, Youth & Families", value: 349 },
+                { name: "Early Childhood", value: 336 },
+                { name: "Human Rights", value: 44 },
+                { name: "Child Support Services", value: 13 },
+                { name: "Status of Women", value: 11 }
+            ]
+        },
+        {
+            name: "Not assigned",
+            children: [
+                { name: "General Responsibilities", value: 1700 },
+                { name: "City Admin.", value: 620 },
+                { name: "Subcategory 3", value: 250 },
+                { name: "Subcategory 4", value: 250 },
+                { name: "Subcategory 5", value: 250 },
+                { name: "Subcategory 6", value: 100 },
+                { name: "Subcategory 7", value: 100 },
+                { name: "Subcategory 8", value: 50 },
+                { name: "Subcategory 9", value: 50 },
+                { name: "Subcategory 10", value: 50 },
+                { name: "Subcategory 11", value: 50 },
+                { name: "Subcategory 12", value: 50 },
+                { name: "Subcategory 13", value: 50 },
+                { name: "Subcategory 14", value: 25 },
+                { name: "Subcategory 15", value: 25 },
+                { name: "Subcategory 16", value: 25 },
+                { name: "Subcategory 17", value: 10 },
+                { name: "Subcategory 18", value: 10 },
+                { name: "Subcategory 19", value: 10 },
+            ]
+        }
+    ]
+};
