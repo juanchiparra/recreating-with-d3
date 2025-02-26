d3228c17d149a8e7855ae910f0512fa9b710545e
Juanchi Parra <juanchiparra@gmail.com>
2025-02-26 10:33:47 -0400

    Create stacked-bar.js

---
+++ docs/2025/W23/stacked-bar.js
@@ -0,0 +1,8 @@
+export const data = [
+    { value: 50, value2: 50, value3: 0, date: "2020" },
+    { value: 60, value2: 55, value3: 0, date: "2021" },
+    { value: 77, value2: 0, value3: 5, date: "2022" },
+    { value: 90, value2: 0, value3: 0, date: "2023" },
+    { value: 110, value2: 0, value3: 0, date: "2024" },
+    { value: 130, value2: 0, value3: 0, date: "2025" }
+];
