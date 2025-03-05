b9d00914cd9dee6bf92585a10a89b1b13e18be5f
Juanchi Parra <juanchiparra@gmail.com>
2025-03-05 09:32:27 -0400

    Create donut-chart.js

---
+++ docs/2025/W24/donut-chart.js
@@ -0,0 +1,7 @@
+export const data = [
+    { value: 45, name: "SPD", color: "#f44647"},
+    { value: 26, name: "CDU", color: "#333333"},
+    { value: 25, name: "Grüne", color: "#46962b"},
+    { value: 15, name: "Linke", color: "#c72767"},
+    { value: 10, name: "AfD", color: "#8b4513"},
+];
