9fc01c2bbd56f3960fadc1500cb2b76a987a2452
Juanchi Parra <juanchiparra@gmail.com>
2025-06-04 09:56:23 -0400

    Create bar-chart.js

---
+++ docs/2025/W29/bar-chart.js
@@ -0,0 +1,12 @@
+export const data = [
+	{ name: "João", value: 1, value2: 22 },
+	{ name: "Gregório", value: 1, value2: 15 },
+	{ name: "Bento*", value: 2, value2: 13 },
+	{ name: "Clemente", value: 0, value2: 14 },
+	{ name: "Inocêncio", value: 0, value2: 13 },
+	{ name: "Leão", value: 1, value2: 12 },
+	{ name: "Pio", value: 4, value2: 8 },
+	{ name: "Estêvão", value: 0, value2: 9 },
+	{ name: "Bonifácio", value: 0, value2: 8 },
+	{ name: "Urbano", value: 0, value2: 8 },
+]
