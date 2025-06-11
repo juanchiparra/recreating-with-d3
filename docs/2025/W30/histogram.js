586a5bd953ca1854859f5bded43a63b521049ac1
Juanchi Parra <juanchiparra@gmail.com>
2025-06-11 16:34:12 -0400

    Create histogram.js

---
+++ docs/2025/W30/histogram.js
@@ -0,0 +1,27 @@
+export const data1 = [
+    { category: '0', value: 3, type: 'light' },
+    { category: '10', value: 12, type: 'light' },
+    { category: '20', value: 18, type: 'light' },
+    { category: '30', value: 20, type: 'light' },
+    { category: '40', value: 17, type: 'dark' },
+    { category: '50', value: 14, type: 'dark' },
+    { category: '60', value: 9, type: 'dark' },
+    { category: '70', value: 5, type: 'dark' },
+    { category: '80', value: 2, type: 'dark' },
+    { category: '90', value: 0, type: 'dark' },
+    { category: '100', value: 0, type: 'dark' }
+];
+
+export const data2 = [
+    { category: '0', value: 5, type: 'dark' },
+    { category: '10', value: 4, type: 'dark' },
+    { category: '20', value: 5, type: 'dark' },
+    { category: '30', value: 7, type: 'dark' },
+    { category: '40', value: 11, type: 'dark' },
+    { category: '50', value: 16, type: 'dark' },
+    { category: '60', value: 20, type: 'light' },
+    { category: '70', value: 19, type: 'light' },
+    { category: '80', value: 11, type: 'light' },
+    { category: '90', value: 3, type: 'light' },
+    { category: '100', value: 0, type: 'light' }
+];
